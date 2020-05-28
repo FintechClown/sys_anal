9a91928368836247eafd535d89865c8b796b2613
FintechClown <65677527+FintechClown@users.noreply.github.com>
2020-05-28 17:24:16 +0800

    提交專案
    提交專案

---
+++ commitProject/app/src/main/java/com/example/commitproject/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.commitproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Main2Activity.this,"成功",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Main2Activity.this,Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ commitProject/app/src/main/java/com/example/commitproject/Main3Activity.java
@@ -0,0 +1,36 @@
+package com.example.commitproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Button button = (Button)findViewById(R.id.button2);
+        EditText tv =(EditText)findViewById(R.id.editText6);
+
+        final String str = tv.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (str.equals("1")) {
+                    Toast.makeText(Main3Activity.this, "成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Main3Activity.this, "失敗", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
